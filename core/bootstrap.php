5a53d7cc506ef20623c73f33abfc9e81be443369
Serwach <mateusz.serwach.1@gmail.com>
2020-03-03 15:08:14 +0100

    classes autoloading added

---
+++ core/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+//require_once(ROOT . DS . 'core' . DS . 'Router.php');
+//require_once 'config/config.php';
+//require_once 'app/libs/helpers/functions.php';
+
+function __autoload($className)
+{
+    if(file_exists(ROOT . DS . 'core' . DS . $className . '.php'))
+    {
+        require_once(ROOT . DS . 'core' . DS . $className . '.php');
+    }
+    elseif(file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php'))
+    {
+        require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+    }
+    elseif(file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php'))
+    {
+        require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
+    }    
+}
+
+Router::route($url);
+
